c1e7e4d5005e2e4c17069ae1011b8fa89f258d6f
Goast <146421288+Venkateshk337@users.noreply.github.com>
2024-02-07 22:07:10 +0530

    Delete common_if_else/odd_even.c

--- common_if_else/odd_even.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int n;
-    printf("Enter the number:");
-    scanf("%d",&n);
-    if(n%2==0)
-    printf("Enter number is even");
-    else printf("Enter number is odd");
-}
